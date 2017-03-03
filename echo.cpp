305efe39ca76a0f99377fe12bd2ef3682caddeb0
Victor Denisov <denisovenator@gmail.com>
2017-03-03 07:01:57 +0000

    Allocate counter globally

--- echo.cpp
+++ echo.cpp
@@ -7,20 +7,20 @@
 using unconsumed_remainder = std::experimental::optional<temporary_buffer<char>>;
 
 struct Consumer {
+    int * counter;
     output_stream<char> *output;
-    Consumer(output_stream<char> *_output) :output(_output) {
+    Consumer(int * _counter, output_stream<char> *_output) :counter(_counter), output(_output) {
     }
 
     future<unconsumed_remainder> operator()(temporary_buffer<char> buf) {
         if (buf) {
             std::copy(buf.begin(), buf.end(), std::ostream_iterator<char>(std::cout));
             std::cout.flush();
-            return smp::submit_to(0, [] {
-                    static int counter = 0;
-                    return ++counter;
+            return smp::submit_to(0, [&] {
+                    return ++(*counter);
                     }).then([&] (int value) {
                         std::stringstream s;
-                        s << "We received this value of the counter " << value;
+                        s << "We received this value of the counter " << value << std::endl;
                         output->write(s.str()).then([&] () { 
                                 return output->flush();
                         });
@@ -35,11 +35,11 @@ struct Consumer {
 };
 
 future<>
-handle_connection (connected_socket s, socket_address a) {
+handle_connection (int * counter, connected_socket s, socket_address a) {
     input_stream<char> input = s.input();
     output_stream<char> output = s.output();
-    return do_with(std::move(s), std::move(input), std::move(output), [] (connected_socket &ss, input_stream<char> &input, output_stream<char> &output) {
-        auto consumer = Consumer(&output);
+    return do_with(std::move(s), std::move(input), std::move(output), [counter] (connected_socket &ss, input_stream<char> &input, output_stream<char> &output) {
+        auto consumer = Consumer(counter, &output);
         return do_with(std::move(consumer), std::move(input), [] (auto &my_consumer, auto &input) {
             return input.consume(my_consumer);
                 });
@@ -47,22 +47,23 @@ handle_connection (connected_socket s, socket_address a) {
 }
 
 future<>
-main_async() {
-    return do_with(listen(make_ipv4_address({1234})), [] (auto& listener) {
-        return keep_doing([&listener] () {
-            return listener.accept().then([] (connected_socket s, socket_address a) {
-                handle_connection(std::move(s), std::move(a));
+main_async(int * counter) {
+    return do_with(listen(make_ipv4_address({1234})), [counter] (auto& listener) {
+        return keep_doing([&listener, counter] () {
+            return listener.accept().then([counter] (connected_socket s, socket_address a) {
+                handle_connection(counter, std::move(s), std::move(a));
             });
         });
     });
 }
 
 int main(int argc, char** argv) {
+    int counter = 0;
     app_template app;
     try {
-        app.run(argc, argv, main_async);
+        app.run(argc, argv, [&counter] {return main_async(&counter);});
     } catch(std::runtime_error &e) {
-        std::cerr << "Couldn't start application: " << e.what() << "\n";
+        std::cerr << "Couldn't start application: " << e.what() << std::endl;
         return 1;
     }
     return 0;
