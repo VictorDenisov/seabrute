f9cdeb9a2e3ed73984040231c94e399b850feb41
Victor Denisov <denisovenator@gmail.com>
2017-04-14 07:02:38 +0000

    Create generator for tasks, it works

--- echo.cpp
+++ echo.cpp
@@ -33,11 +33,13 @@ struct config {
 private:
     static const std::string default_alph;
     static const std::string default_hash;
-    static const int default_length = 4;
+    static const int default_length;
 };
 
-const std::string config::default_alph = std::string("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+//const std::string config::default_alph = std::string("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+const std::string config::default_alph = std::string("csit");
 const std::string config::default_hash = std::string("eMWCfUJDk9Lec");
+const int config::default_length = 4;
 
 /********************/
 
@@ -46,26 +48,65 @@ struct task {
     int from, to;
     std::string hash;
     std::string password;
-    task(std::string alph, int from, int to, std::string hash, std::string password) :
+    task(std::string alph, int from, int to, std::string hash, std::string password) noexcept :
         alph(alph), from(from), to(to), hash(hash), password(password) {}
+    task(const task &rvalue) noexcept :
+        alph(rvalue.alph), from(rvalue.from), to(rvalue.to), hash(rvalue.hash), password(rvalue.password) {}
+    task(const task &&rvalue) noexcept :
+        alph(rvalue.alph), from(rvalue.from), to(rvalue.to), hash(rvalue.hash), password(rvalue.password) {}
+    task& operator=(const task& rvalue) {
+        alph = rvalue.alph;
+        from = rvalue.from;
+        to = rvalue.to;
+        hash = rvalue.hash;
+        password = rvalue.password;
+        return *this;
+    }
 };
 
 class task_generator {
     task current;
+    std::vector<unsigned int> index;
+    bool started, finished;
 public:
-    task_generator(task &&task) : current(task) {}
-    task get_next() {
-        return current;
+    task_generator(task &&task) : current(task), index(task.to - task.from), started(false), finished(false) {
+        int len = current.to - current.from;
+        if (len == 0)
+            finished = true;
+        current.password.replace(current.from, len, len, current.alph[0]);
+    }
+    boost::optional<task> get_next() {
+        if (finished)
+            return boost::none;
+        if (not started) {
+            started = true;
+            return current;
+        }
+
+        auto pass_shift = current.password.length() - current.to;
+        auto pos = current.password.rbegin() + pass_shift;
+        for (auto i = index.rbegin(); i != index.rend(); ++i, ++pos) {
+            if (*i == current.alph.length() - 1) {
+                *i = 0;
+                *pos = current.alph[0];
+            } else {
+                ++*i;
+                *pos = current.alph[*i];
+                return current;
+            }
+        }
+        finished = true;
+        return boost::none;
     }
 };
 
 using unconsumed_remainder = std::experimental::optional<temporary_buffer<char>>;
 
 struct consumer {
-    task_generator *tsk_gen;
+    std::shared_ptr<task_generator> tsk_gen;
     output_stream<char> *output;
 
-    consumer(task_generator *_tsk_gen, output_stream<char> *_output) : tsk_gen(_tsk_gen), output(_output) {
+    consumer(std::shared_ptr<task_generator> _tsk_gen, output_stream<char> *_output) : tsk_gen(_tsk_gen), output(_output) {
     }
 
     future<unconsumed_remainder> operator()(temporary_buffer<char> buf) {
@@ -74,9 +115,13 @@ struct consumer {
             std::cout.flush();
             return smp::submit_to(0, [&] {
                 return tsk_gen->get_next();
-            }).then([&] (task t) {
+            }).then([&] (boost::optional<task> ot) {
                 std::stringstream s;
-                s << "We received this value of the counter " << std::endl;
+                if (ot) {
+                    s << "We received this value of the counter " << ot->password << std::endl;
+                } else {
+                    s << "We didn't receive any value." << std::endl;
+                }
                 output->write(s.str()).then([this] () {
                     return output->flush();
                 });
@@ -90,7 +135,7 @@ struct consumer {
 };
 
 future<>
-handle_connection (task_generator *tsk_gen, connected_socket s, socket_address a) {
+handle_connection (std::shared_ptr<task_generator> tsk_gen, connected_socket s, socket_address a) {
     input_stream<char> input = s.input();
     output_stream<char> output = s.output();
     return do_with(std::move(input), std::move(output), [tsk_gen] (input_stream<char> &input, output_stream<char> &output) {
@@ -102,7 +147,7 @@ handle_connection (task_generator *tsk_gen, connected_socket s, socket_address a
 }
 
 future<>
-main_async(task_generator *tsk_gen) {
+main_async(std::shared_ptr<task_generator> tsk_gen) {
     listen_options lo;
     lo.reuse_address = true;
     return do_with(listen(make_ipv4_address({1234}), lo), [tsk_gen] (auto& listener) {
@@ -123,9 +168,9 @@ int main(int argc, char** argv) {
     try {
         app.run(argc, argv, [&app] {
             auto config = seabrute::config(app.configuration());
-            seabrute::task_generator tsk_gen(seabrute::task(config.alph, 0, config.length, config.hash, std::string(config.length, '\0')));
-            return parallel_for_each(boost::irange<unsigned int>(0, smp::count), [&tsk_gen] (unsigned int core) {
-                return smp::submit_to(core, [&tsk_gen] {return main_async(&tsk_gen);});
+            auto tsk_gen = std::make_shared<seabrute::task_generator>(seabrute::task(config.alph, 0, config.length, config.hash, std::string(config.length, '\0')));
+            return parallel_for_each(boost::irange<unsigned int>(0, smp::count), [tsk_gen] (unsigned int core) {
+                return smp::submit_to(core, [tsk_gen] {return main_async(tsk_gen);});
             });
         });
     } catch(std::runtime_error &e) {
